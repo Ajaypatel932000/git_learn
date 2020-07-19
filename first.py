37749ecb30243d69ba57abe491cbcbb2a927cbb4
AJAY <ap210484@gmail.com>
2020-07-19 12:50:45 +0530

    third changes

--- first.py
+++ first.py
@@ -3,4 +3,5 @@ def sum(a,b):
 
 n1 =int(input("Enter first number = "))
 n2 =int(input("Enter Second number ="))
+print("=======================================")
 print("Addition of two number is =",sum(n1,n2))
