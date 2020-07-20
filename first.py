08ee5b69141dc9c26062a6ae3bf32d878fb758a5
AJAY <ap210484@gmail.com>
2020-07-20 17:25:53 +0530

    add second file

--- first.py
+++ first.py
@@ -5,9 +5,5 @@ def sum(a, b):
 n1 = int(input("Enter first number =>"))
 n2 = int(input("Enter Second number => "))
 print("=======================================")
-
-
-print("-------------------------------------------")
-print("=======================================")
 print("=======================================")
 print("Addition of two number is =", sum(n1, n2))
